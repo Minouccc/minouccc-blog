a67b8d37c30fe538bbd5c129302f5a2e5c02a17e
Minouccc <343723899@qq.com>
2025-01-19 20:21:33 +0800

    fix: projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,17 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'React Playground',
+    description: `该项目提供一个在线的可交互编辑环境，页面左边编写代码，右边实时预览。支持拖拽改变宽度、
+    格式化代码、主题切换、链接分享代码、以及代码下载等功能。比如开发的组件库，可以用这个 React Playground 来实现组件的在线预览。`,
+    imgSrc: '/static/images/projects/project_rp.png',
+    href: 'https://react-playground.minouccc.online/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'PDF旋转工具',
+    description: `PDF旋转，支持PDF文件上传，旋转，下载。`,
+    imgSrc: '/static/images/projects/project_pdf.png',
+    href: 'https://pdf-rotate.minouccc.online',
   },
 ]
 
